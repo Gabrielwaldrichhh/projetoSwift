bd93c69fc4de997cc54ed921494f8f6a4e714852
Gabriel <Gabriel-waldrich@hotmail.coms-iMac.local>
2020-02-18 08:27:47 -0300

    Dei conta

---
+++ myFirstApp/MapScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapScreenViewController.swift
+//  myFirstApp
+//
+//  Created by Jonathan on 17/02/20.
+//  Copyright © 2020 HBSIS. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+}
---
+++ myFirstApp/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  myFirstApp
+//
+//  Created by Jonathan on 17/02/20.
+//  Copyright © 2020 HBSIS. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView! 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
